edb7e80d1dee81a5208b2afe4db98504da6f4d7e
HZ <beholdthekobold@gmail.com>
2020-12-26 17:03:24 +0300

    created project for courseinfo exercise

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
